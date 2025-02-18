0e769354be8054bf5a8517e54a362fd60fecb3df
Manas8114 <119348358+Manas8114@users.noreply.github.com>
2025-02-18 15:27:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+ import TemperatureConverter from "./components/TemperatureConverter";
+/* import DarkModeToggle from "./components/DarkModeToggle";  
+ *//* import UserProfileForm from "./components/UserProfileForm";
+ */
+function App() {
+  return (
+    <div>
+      <h1>Project 3 Manas.</h1>
+       <TemperatureConverter /> 
+      {/* <DarkModeToggle/>  */}
+     {/*  <UserProfileForm /> */}
+    </div>
+  );
+}
+
+export default App;
+
+
+/* we have to change the importd and div components to activate different different componoets. */
